3ba103cfd2d456598a71556d9bd35a6ef3797366
Rob <rob.luton@gmail.com>
2019-11-29 19:22:39 -0600

    add redux dev tools

--- src/store.js
+++ src/store.js
@@ -2,12 +2,24 @@
  * src/store.js
  * No initialState
  */
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/';
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+  // other store enhancers if any
+);
+
 export default function configureStore() {
   return createStore(
     rootReducer,
-    applyMiddleware(thunk)
+    enhancer,
   );
 }
